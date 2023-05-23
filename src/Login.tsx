82b2927b1b6c81d58fe7536ebac4bf5894e0cdff
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2023-05-22 21:22:22 -0400

    adding eslint and prettier

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,39 +1,47 @@
-
-
 function Login() {
   return (
     <>
-    <div className="h-max">
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <div className="card-body">
-    <img src="src/assets/logo_kse.jpg" className="mb-4 rounded-md shadow-md" />
-    <div className="form-control w-full max-w-xs">
-  <label className="label">
-    <span className="label-text">Email</span>
-  </label>
-  <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-</div>
-<div className="form-control w-full max-w-xs">
-  <label className="label">
-    <span className="label-text">Password</span>
-  </label>
-  <input type="password" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-</div>
-<div className="form-control ">
-  <label className="label cursor-pointer justify-start gap-5">
-  <input type="checkbox" className="checkbox checkbox-primary" />
-  <span className="label-text">Remember me</span> 
-  </label>
-</div>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary grow">Login</button>
-    </div>
-  </div>
-</div>
-    </div>
-
+      <div className="h-max">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <img
+              src="src/assets/logo_kse.jpg"
+              className="mb-4 rounded-md shadow-md"
+            />
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div className="form-control ">
+              <label className="label cursor-pointer justify-start gap-5">
+                <input type="checkbox" className="checkbox checkbox-primary" />
+                <span className="label-text">Remember me</span>
+              </label>
+            </div>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary grow">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
